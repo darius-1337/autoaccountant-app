2d496941db7b8dfd824c95f9a944f4589a5128ef
Darius Tiganas <adarius9.1999@gmail.com>
2026-07-24 04:37:43 +0200

    added the current fiscal year and the constant meal limits (dietas)

---
+++ src/main/java/io/github/darius/autoaccountant/domain/FiscalConstants.java
@@ -0,0 +1,9 @@
+package io.github.darius.autoaccountant.domain;
+
+public class FiscalConstants {
+    public static final int CURRENT_FISCAL_YEAR = 2026;
+    public static final double MEAL_LIMIT_SPAIN_NO_OVERNIGHT = 26.67;
+    public static final double MEAL_LIMIT_SPAIN_OVERNIGHT = 53.34;
+}
+
+/* MEAL LIMIT se refiere a dietas */
